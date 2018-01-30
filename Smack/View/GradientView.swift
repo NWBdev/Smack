b293b3ee970dd05f3d9ebeb8a2a57c2d45220cdd
Nathaniel Burciaga <nathanielburciaga@nathaniels-mbp>
2018-01-30 00:07:28 -0800

    Updated GradientViewController, ChannelView, ChatView UI

---
+++ Smack/View/GradientView.swift
@@ -0,0 +1,40 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Nathaniel Burciaga on 1/29/18.
+//  Copyright © 2018 Nathaniel Burciaga. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable //render in storyboard
+class GradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.2901960784, green: 0.3019607843, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout() // invalids the current layout!
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout() // invalids the current layout!
+        }
+    }
+
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        
+        self.layer.insertSublayer(gradientLayer, at: 0) // at the first layer of gradient
+    }
+    
+    
+    
+    
+}
