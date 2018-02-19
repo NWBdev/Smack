dc329acb39427d3cb14d91a7b3f37a25d92cc20a
Nathaniel Burciaga <nathanielburciaga25@gmail.com>
2018-02-18 22:27:57 -0800

    added ProfileVC and Finished ProfileVC

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -44,7 +44,16 @@ class ChannelVC: UIViewController {
     
     
     @IBAction func loginBtnPressed(_ sender: Any) {
-        performSegue(withIdentifier: TO_LOGIN, sender: nil)
+        
+        if AuthService.instance.isLoggedIn {
+            //Show Profile Page
+            let profile = ProfileVC()
+            profile.modalPresentationStyle = .custom
+            present(profile, animated: true, completion: nil)
+        }else {
+            performSegue(withIdentifier: TO_LOGIN, sender: nil)
+
+        }
     }
     
     
